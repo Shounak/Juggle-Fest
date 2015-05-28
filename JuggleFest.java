06c44d695d309b0f2c60bab611d2d5e6bcb100fd
unknown <shounakkul@gmail.com>
2015-05-28 11:11:48 -0500

    Added Juggler class

--- JuggleFest.java
+++ JuggleFest.java
@@ -1,6 +1,9 @@
 public class JuggleFest
 {
-
+	public static void main(String[] args) 
+	{
+		
+	}
 }
 
 class Circuit
@@ -36,7 +39,41 @@ class Circuit
 	}
 }
 
-class Jugggler
+class Juggler
 {
+	private String name;
+	private int handEyeCoordination;
+	private int endurance;
+	private int pizzazz;
+	private String[] circuitPreferences;
 
+	public Juggler(String n, int h, int e, int p, String[] circuits)
+	{
+		name = n;
+		handEyeCoordination = h;
+		endurance = e;
+		pizzazz = p;
+		circuitPreferences = circuits;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+	public int getHandEyeCoordination()
+	{
+		return this.handEyeCoordination;
+	}
+	public int getEndurance()
+	{
+		return this.endurance;
+	}
+	public int getPizzazz()
+	{
+		return this.pizzazz;
+	}
+	public String[] getCircuitPreferences()
+	{
+		return this.circuitPreferences;
+	}
 }
