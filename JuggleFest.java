2d290872df512365822e593ebf7b941293aa2c4f
Shounak <shounakkul@gmail.com>
2015-05-29 00:00:14 -0500

    Changed from HashMap to ArrayList implementation

--- JuggleFest.java
+++ JuggleFest.java
@@ -32,23 +32,17 @@ public class JuggleFest
 		return (linesCount-1); // "-1" is because there is an empty line between Circuits and Jugglers
 	}
 
-	static HashMap<Circuit, ArrayList<Juggler>> readFile(String fileName)
+	static ArrayList<Circuit> readFile(String fileName)
 	{
 		BufferedReader fileReader = null;
-
-		// A Hashmap where Keys are Circuits and Values are Jugglers
-		// HashMap is used for its O(1) lookup and as a natural way of organizing circuits and their lists of jugglers 
-		HashMap<Circuit, ArrayList<Juggler>> jugglerAssignments = new HashMap<>(); 
-
-		// Maps string circuit names to circuit objects e.x. "C0" => C C0 H:7 E:7 P:10
-		HashMap<String, Circuit> circuits = new HashMap<>();
+		ArrayList<Circuit> circuits = new ArrayList<>();
 
 		int numberOfCircuits = 0;
 		try
 		{
 			fileReader = new BufferedReader(new FileReader(fileName));
 
-			// read in all the circuits
+			// read in all the circuits and add them to the ArrayList
 			String circuitLine = fileReader.readLine();
 			while (!(circuitLine.trim().equals("")) && circuitLine.charAt(0) == 'C') 
 			{	
@@ -59,8 +53,7 @@ public class JuggleFest
 				int newCircuitEndurance = Integer.parseInt(data[3].split(":")[1]);
 				int newCircuitPizzazz = Integer.parseInt(data[4].split(":")[1]);
 				Circuit newCircuit = new Circuit(newCircuitName, newCircuitHandEyeCoordination, newCircuitEndurance, newCircuitPizzazz);
-				jugglerAssignments.put(newCircuit, new ArrayList<Juggler>());
-				circuits.put(newCircuitName, newCircuit);
+				circuits.add(newCircuit);
 				numberOfCircuits++;	
 				circuitLine = fileReader.readLine();
 			}
@@ -72,7 +65,7 @@ public class JuggleFest
 			while ((jugglerLine = fileReader.readLine()) != null)
 			{
 				String[] data = jugglerLine.split(" ");
-				placeJuggler(jugglerAssignments, circuits, data, jugglersPerTeam);
+				placeJuggler(circuits, data, jugglersPerTeam);
 			}
 		}
 		catch (IOException e) 
@@ -91,29 +84,28 @@ public class JuggleFest
 			}
 		}
 
-		return jugglerAssignments;
+		return circuits;
 	}
 
-	private static void placeJuggler(HashMap<Circuit, ArrayList<Juggler>>  jugglersMap, HashMap<String, Circuit> circuitsMap, String[] abilities, int teamSize)
+	private static void placeJuggler(ArrayList<Circuit>  circuitsList, String[] abilities, int teamSize)
 	{
 		String newJugglerName = abilities[1];
 		int newJugglerHandEyeCoordination = Integer.parseInt(abilities[2].split(":")[1]);
 		int newJugglerEndurance = Integer.parseInt(abilities[3].split(":")[1]);
 		int newJugglerPizzazz = Integer.parseInt(abilities[4].split(":")[1]);
 		String[] circuitPrefs = abilities[5].split(",");
-		jugglersMap.get(circuitsMap.get(circuitPrefs[0])).add(new Juggler(newJugglerName, newJugglerHandEyeCoordination, newJugglerEndurance, newJugglerPizzazz, circuitPrefs));
+
+		int circuitsListIndex = Character.getNumericValue(circuitPrefs[0].charAt(1));
+		circuitsList.get(circuitsListIndex).jugglers.add(new Juggler(newJugglerName, newJugglerHandEyeCoordination, newJugglerEndurance, newJugglerPizzazz, circuitPrefs));
 	}
 
-	static void output(HashMap<Circuit, ArrayList<Juggler>>  jugglersAndCircuits)
+	static void output(ArrayList<Circuit>  jugglersAndCircuits)
 	{
-		Set set = jugglersAndCircuits.entrySet();
-		Iterator i = set.iterator();
-
-		while(i.hasNext()){
-			Map.Entry<Circuit, ArrayList<Juggler>> entry = (Map.Entry)i.next();
-			System.out.print(entry.getKey().getName());
-			for (Juggler juggler : entry.getValue())
-				System.out.print(" " + juggler.getName());
+		for (Circuit c : jugglersAndCircuits)
+		{
+			System.out.print(c.getName());
+			for (Juggler j : c.jugglers)
+				System.out.print(" " + j.getName());
 			System.out.println("");
 		}
 	}
@@ -130,6 +122,7 @@ class Circuit
 	private int handEyeCoordination;
 	private int endurance;
 	private int pizzazz;
+	public ArrayList<Juggler> jugglers = new ArrayList<>();
 
 	public Circuit(String n, int h, int e, int p)
 	{
@@ -154,47 +147,6 @@ class Circuit
 	public int getPizzazz()
 	{
 		return this.pizzazz;
-	}
-
-	// Overriding .equals and .hashCode is necessary for inserting Circuits as a Key into the HashMap
-	@Override
-	public boolean equals(Object other) 
-	{
-		if (!(other instanceof Circuit)) 
-		{
-			return false;
-		}
-		Circuit c = (Circuit) other;
-		return equalsHelper(name, c.name) && 
-		equalsHelper(handEyeCoordination, c.handEyeCoordination) &&
-		equalsHelper(endurance, c.endurance) &&
-		equalsHelper(pizzazz, c.pizzazz);
-	}
-
-	private static boolean equalsHelper(Object a, Object b) 
-	{
-		if (a == null || b == null) 
-		{
-			return false;
-		}
-		if (a == b) 
-		{
-			return true;
-		}
-
-		return a.equals(b);
-	}
-
-
-	@Override
-	public int hashCode() 
-	{
-		int hash = 17;
-		hash = hash * 31 + (name == null ? 0 : name.hashCode());
-		hash = hash * 31 + handEyeCoordination;
-		hash = hash * 31 + endurance;
-		hash = hash * 31 + pizzazz;
-		return hash;
 	}
 }
 
